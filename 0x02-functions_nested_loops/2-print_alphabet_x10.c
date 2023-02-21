42ea6df106389f19f42bdb42d448ee8237ab6d37
Jenkins-05 <kennedynjobvu5@gmail.com>
2023-02-21 06:38:33 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * prints 10 times the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10- prints 10 times the alphabet,
+ * in lowercase, followed by a new line
  *
  */
 void print_alphabet_x10(void)
 {	int i;
 
-	for (i = 0; i<= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{	char letter;
 
-		for ( letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
 	}
