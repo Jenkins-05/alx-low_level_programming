9219ee73df09dccde47dd31e3d51dd54d64645f4
Jenkins-05 <kennedynjobvu5@gmail.com>
2023-02-23 11:25:23 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,21 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int j;
+	char n, c;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i < 10)
 	{
-		for (j = 0; j < 15; j++)
+		for (n = 0; n <= 14; n++)
 		{
-			if (j < 9)
+			c = n;
+
+			if (n > 9)
 			{
-				_putchar((j / 10) + '0');
-				_putchar(j % 10 + '0');
+				_putchar('1');
+				c = n % 10;
 			}
 		}
-		_putchar('\n');
+		_putchar('0' + c);
 	}
 }
