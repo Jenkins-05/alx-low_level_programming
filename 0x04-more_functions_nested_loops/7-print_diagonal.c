894f5b5382b49bf32063a2504901b4195107fb4e
Jenkins-05 <kennedynjobvu5@gmail.com>
2023-02-23 10:06:25 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,20 +19,15 @@ void print_diagonal(int n)
 	else
 	{
 
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= i; j++)
 			{
-				if (j == i)
-				{
-					_putchar(92);
-				}
-				else if (j < i)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
+
 			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
