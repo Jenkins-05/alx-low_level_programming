70325a29ce02c7a272368bcebfd1b3f38e25b1f8
Jenkins-05 <kenneddynjobvu5@gmail.com>
2023-03-02 14:03:24 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int index, dest_len;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-	j++;
-}
+	index = 0;
+	dest_len = 0;
 
-dest[i] = '\0';
-return (dest);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
