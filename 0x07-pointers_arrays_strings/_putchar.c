da5622a048f97163764dde69dc7a72a055d0cf8c
Jenkins-05 <kenneddynjobvu5@gmail.com>
2023-03-05 23:08:04 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd>
+
+/**
+ *_putchar - prints character on stdout
+ *@ch: character to be printed
+ *Return:1 (on success)
+ *Otherwise 0
+ *
+ */
+char _putchar(char ch)
+{
+	return (write(1, &ch, 1));
+}
