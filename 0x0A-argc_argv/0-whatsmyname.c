2dfe84f92492451b8cf8d08c13cf114ca448d8f6
Jenkins-05 <kenneddynjobvu5@gmail.com>
2023-03-10 04:34:54 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_whatsmyname - funtion that prints program name
+ *
+ *@argc:argument count
+ *
+ *@argv:argument vector
+ *
+ *Return: 0 (on Success)
+ *
+ */
+int _whatsmyname(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+	return (0);
+}
